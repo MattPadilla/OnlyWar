0377402dc926af6db7c29087aeaf73ef6de245d9
UltraLightning <matthewpadilla02@gmail.com>
2023-06-16 02:00:45 -0400

    Add files via upload

---
+++ desktop/src/com/onlywar/game/DesktopLauncher.java
@@ -0,0 +1,33 @@
+package com.onlywar.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.onlywar.game.OnlyWar;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	
+	
+	
+	public static void main (String[] arg) {
+		
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		
+		OnlyWar game = new OnlyWar();
+	
+		
+		config.setTitle("only-war");
+		// Max 1480, 888
+		config.setWindowedMode(800, 480);
+		
+		config.useVsync(true);
+		
+		config.setForegroundFPS(60);
+		
+	
+		
+		new Lwjgl3Application(game, config);
+		
+		
+	}
+}
